8b846b6daf4e0c2eb6e042b9feedb88523b308c8
SiqiRan <rsq-work@outlook.com>
2023-03-03 10:06:35 +0800

    [Siqi][refactor]: delete unnecessary field subordinates in ParkingDirector.

--- src/main/java/parking/lot/entity/directors/ParkingDirector.java
+++ src/main/java/parking/lot/entity/directors/ParkingDirector.java
@@ -10,11 +10,9 @@ import parking.lot.entity.operators.Valet;
 @Getter
 @Setter
 public class ParkingDirector {
-    private List<Valet> subordinates;
     private ParkingTable parkingTable;
 
     public ParkingDirector(List<Valet> subordinates) {
-        this.subordinates = subordinates;
-        this.parkingTable = new ParkingTable(this.subordinates);
+        this.parkingTable = new ParkingTable(subordinates);
     }
 }
